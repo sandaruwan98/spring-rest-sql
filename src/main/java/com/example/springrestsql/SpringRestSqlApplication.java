cb07ade4c9be7b306f1c1561dc8478afd7c67217
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-05-04 19:24:45 +0530

    initial commit

---
+++ src/main/java/com/example/springrestsql/SpringRestSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrestsql/SpringRestSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
