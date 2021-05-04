b6cc1f306f53dbd84040be3301173c33bcda377e
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2021-05-04 20:48:43 +0530

    getal and adduser requests

--- src/main/java/com/example/springrestsql/User/UserController.java
+++ src/main/java/com/example/springrestsql/User/UserController.java
@@ -1,14 +1,25 @@
 package com.example.springrestsql.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "user")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
     @GetMapping
-    public @ResponseBody String heloo(){
-        return "hello";
+    public List<User> getUsers(){
+        return userService.getUsers();
     }
+
+    @PostMapping
+    public void addUser(@RequestBody User user){
+        userService.addUser(user);
+    }
+
 
 }
--- src/main/java/com/example/springrestsql/User/UserService.java
+++ src/main/java/com/example/springrestsql/User/UserService.java
@@ -1,4 +1,20 @@
 package com.example.springrestsql.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    @Autowired
+    private  UserRepository userRepository;
+
+    List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
 }
